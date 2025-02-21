0aafbd2c82d89a867659a05b049978645f28d07b
Andrew Loeliger <loeliger@wisc.edu>
2025-02-20 21:30:31 -0600

    Add CICADA v2.2.0

---
+++ CICADA_v2p2p0/weights/b10.h
@@ -0,0 +1,17 @@
+//Numpy array shape [1]
+//Min 0.000000000000
+//Max 0.000000000000
+//Number of zeros 1
+
+#ifndef B10_H_
+#define B10_H_
+
+namespace CICADA_v2p2p0{
+#ifdef LOAD_WEIGHTS_FROM_TEXT
+  bias10_t b10[1];
+#else
+  bias10_t b10[1] = {0};
+#endif
+}
+
+#endif
---
+++ CICADA_v2p2p0/weights/b7.h
@@ -0,0 +1,17 @@
+//Numpy array shape [16]
+//Min 0.000000000000
+//Max 0.000000000000
+//Number of zeros 16
+
+#ifndef B7_H_
+#define B7_H_
+
+namespace CICADA_v2p2p0{
+#ifdef LOAD_WEIGHTS_FROM_TEXT
+  bias7_t b7[16];
+#else
+  bias7_t b7[16] = {-1.3750, -1.2500, -0.4375, -0.9375, -1.4375, -0.7500, -2.0625, -1.3750, -7.9375, -1.9375, -1.1250, -0.8125, -1.8125, -1.5000, -1.8750, -1.6250};
+#endif
+}
+ 
+#endif
